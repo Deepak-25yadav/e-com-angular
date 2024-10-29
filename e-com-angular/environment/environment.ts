45e3f305038d16c668f5baf74ac74112c3b324b3
Deepak-25yadav <25yadavdeepak@gmail.com>
2024-10-29 15:00:00 +0530

    updated angular.json

--- e-com-angular/environment/environment.ts
+++ e-com-angular/environment/environment.ts
@@ -1,5 +1,17 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:5500'
+
+    // e-cart-backend >> localhost url means old or first repo of backend
+
+    // baseUrl: 'http://localhost:5500',
+
+    // e-com-angular-backend >> latest localhost backend in this have implemented authorization and 
+    // indexing.
+
+    // baseUrl: 'http://localhost:4500', 
+    
+    // e-cart-backend repo deployed url
+
+    baseUrl: "https://e-cart-backend-04v3.onrender.com"
   };
   
